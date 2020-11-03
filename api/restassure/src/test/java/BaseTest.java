fffb98e6f55578a25e78457daf7c67b5b5bfc2b5
edgaricas <ariasedgar992@gmail.com>
2020-11-02 23:06:28 -0600

    implementacion de restassured

--- api/restassure/src/test/java/BaseTest.java
+++ api/restassure/src/test/java/BaseTest.java
@@ -1,29 +1,23 @@
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 
 public class BaseTest {
-
-    public static RequestSpecification request;
+    public RequestSpecification request;
 
     @BeforeClass
     @Parameters ("baseUrl")
     public void SetupRestAssured(@Optional String baseUrl){
         if (baseUrl == null){
-            baseUrl = "http://localhost:9000";
+            baseUrl = "https://api-coffee-testing.herokuapp.com";
         }
         RestAssured.baseURI = baseUrl;
 
         System.out.println(String.format("Running on %s environment", baseUrl));
     }
 
-    //    this is to clean the request object
     @BeforeMethod
-    public void Setup(){
+    public void before(){
         request = RestAssured.given();
     }
-
 }
